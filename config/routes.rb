209bc79c34f37bf23ab4698603b68057580d7285
summerspirit <naomi.freeman@yahoo.ca>
2013-09-30 14:46:55 +0000

    random routes

--- config/routes.rb
+++ config/routes.rb
@@ -15,11 +15,13 @@ Flickr::Application.routes.draw do
 
   get 'logout', to: "sessions#destroy"
 
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
